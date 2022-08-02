cda14e46d0d02460cdf1508864759d9af8938d5c
STEPHEN ONOKWAKPOR <classicsteph11@gmail.com>
2022-08-02 15:16:00 +0100

    implemented add and display of the items to review on database and home page respectively

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,17 +1,24 @@
-
-
-export default function Header(){
-    return( 
+import Link from "next/link";
+export default function Header() {
+  return (
     <div className="mx-10">
-         <div className="flex flex-col  px-10 py-6 sm:space-y-4  md:flex-row md:justify-between">
-        <h1 className="tracking-wide text-xl text-gray-400 uppercase"><span className="italic text-2xl text-blue-100">Reviews</span> by <span className="text-red-500 font-bold">Stephen</span> </h1>
+      <div className="flex flex-col  px-10 py-6 sm:space-y-4  md:flex-row md:justify-between">
+        <h1 className="tracking-wide text-xl text-gray-400 uppercase">
+          <span className="italic text-2xl text-blue-100">Reviews</span> by{" "}
+          <span className="text-red-500 font-bold">Stephen</span>{" "}
+        </h1>
 
         <div className="auth text-white">
-            <a href="/api/auth/signin" className="bg-black px-6 py-2 capitalize font-bold text-xl border border-white rounded shadow-xl hover:bg-red-500 hover:border-none transition-all">sign in</a>
+          <Link href={`/api/auth/signin`}>
+    
+            <a
+              className="bg-black px-6 py-2 capitalize font-bold text-xl border border-white rounded shadow-xl hover:bg-red-500 hover:border-none transition-all"
+            >
+              sign in
+            </a>
+          </Link>
         </div>
+      </div>
     </div>
-    </div>
-   
-)
-
+  );
 }
